c93fe0184ab7d62a8e847293f4310c43b79ca1c7
Jelte Homminga <jelte.homminga@gmail.com>
2021-03-03 10:08:39 +0100

    finished exercises

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -3,6 +3,27 @@
 
 import * as React from 'react'
 import '../box-styles.css'
+/*
+.box {
+  border: 1px solid #333;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+}
+.box--large {
+  width: 270px;
+  height: 270px;
+}
+.box--medium {
+  width: 180px;
+  height: 180px;
+}
+.box--small {
+  width: 90px;
+  height: 90px;
+}
+*/
 
 // 💰 Use the className for the size and style (backgroundColor) for the color
 // 💰 each of the elements should also have the "box" className applied
@@ -12,16 +33,52 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
+
+const Box = ({children, backgroundColor, size}) => (
+  <div
+    className={`box box--${size}`}
+    style={{backgroundColor, fontStyle: 'italic'}}
+  >
+    {children}
+  </div>
+)
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" backgroundColor="lightblue">
+        small lightblue box
+      </Box>
+      <Box size="medium" backgroundColor="pink">
+        medium pink box
+      </Box>
+      <Box size="large" backgroundColor="orange">
+        large orange box
+      </Box>
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,12 +2,29 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import {useRef, useState} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const inputRef = useRef('')
+  // const [error, setError] = useState('')
+  const [usernameInput, setUsernameInput] = useState('')
+
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    onSubmitUsername(inputRef.current.value)
+    // onSubmitUsername(e.target.elements.usernameInput.value)
+  }
+
+  const handleChange = ({target: {value}}) => {
+    // const isValid = value === value.toLowerCase()
+    // setError(isValid ? null : 'Username must be lower case')
+    setUsernameInput(value.toLowerCase())
+  }
   //
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
@@ -19,10 +36,17 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        {/* <p role="alert">{error}</p> */}
+        <label htmlFor="usernameInput">Username:</label>
+        <input
+          id="usernameInput"
+          type="text"
+          ref={inputRef}
+          onChange={handleChange}
+          value={usernameInput}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
